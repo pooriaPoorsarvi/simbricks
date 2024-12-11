7952cb66d4351adbe1f2719fd90cc1412e7d1873
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2024-12-11 11:48:22 +0000

    updated utils for pods

--- utils/pods/run.sh
+++ utils/pods/run.sh
@@ -2,7 +2,13 @@ cd ../../
 
 rm -r out
 
+START=$(date +%s%3N)
+
 simbricks-run experiments/pyexps/pods/all_memory_configs.py --runs 1 --force 
+
+END=$(date +%s%3N)
+
+echo "Time for all experiments was: $((END - START)) ms"
 
 cd utils/pods
 
